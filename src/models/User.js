23d1fd0d24bc0a051eac2a3548833d0ca483d730
mfghir <fatemeghafari77@gmail.com>
2023-08-18 03:37:00 -0700

    add user module

---
+++ src/models/User.js
@@ -0,0 +1,21 @@
+import { Schema, model, modules } from "mongoose";
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: () => Date.now(),
+    immutable: true,
+  },
+});
+
+const User = modules.User || model("User", userSchema);
+
+export default User;
