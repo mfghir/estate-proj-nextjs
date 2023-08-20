3b471c785f282c5fff65bc4fff257ce5fa6284ee
mfghir <fatemeghafari77@gmail.com>
2023-08-20 01:48:39 -0700

    add SessionProvider

---
+++ src/providers/NextAuthProvider.js
@@ -0,0 +1,8 @@
+"use client"
+import { SessionProvider } from "next-auth/react";
+
+function NextAuthProvider({ children }) {
+  return <SessionProvider>{children}</SessionProvider>;
+}
+
+export default NextAuthProvider;
