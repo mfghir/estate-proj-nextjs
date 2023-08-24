a6cbadb63c3acf62988206d7e8002872bfac9199
mfghir <fatemeghafari77@gmail.com>
2023-08-24 01:56:21 -0700

    create profile api

---
+++ src/app/api/profile/route.js
@@ -0,0 +1,48 @@
+import User from "@/models/User";
+import connectDB from "@/utils/connectDB";
+import { getServerSession } from "next-auth";
+import { NextResponse } from "next/server";
+
+export async function POST(req) {
+  try {
+    await connectDB();
+
+    const body = await req.json();
+    const {
+      title,
+      description,
+      location,
+      phone,
+      price,
+      realState,
+      constructionDate,
+      category,
+      rules,
+      amenities,
+    } = body;
+
+    const session = getServerSession(req);
+    if (!session) {
+      return NextResponse.json(
+        { error: "لطفا وارد حساب کاربری خود شوید" },
+        { status: 401 }
+      );
+    }
+
+    const user = await User.findOne({ email: session.user.email });
+    console.log(user);
+
+    if (!user) {
+      return NextResponse.json(
+        { error: "حساب کاربری یافت نشد" },
+        { status: 404 }
+      );
+    }
+  } catch (err) {
+    console.log(err);
+    return NextResponse.json(
+      { error: "مشکلی در سرور رخ داده است" },
+      { status: 500 }
+    );
+  }
+}
