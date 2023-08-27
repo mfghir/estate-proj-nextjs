36f07b159e89820fa3d46fa22e125d8e1e8a1f1b
mfghir <fatemeghafari77@gmail.com>
2023-08-26 22:54:56 -0700

    add style to card

--- src/components/module/Card.js
+++ src/components/module/Card.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import Link from "next/link";
+import styles from "./Card.module.css";
+import { GiOfficeChair } from "react-icons/gi";
 
-const Card = () => {
+import { BiStore } from "react-icons/bi";
+import { MdApartment } from "react-icons/md";
+import { RiHome3Line } from "react-icons/ri";
+
+import { HiOutlineLocationMarker } from "react-icons/hi";
+import { BiLeftArrowAlt } from "react-icons/bi";
+
+const Card = ({ data: { category, title, location, price } }) => {
+  const icons = {
+    villa: <RiHome3Line />,
+    apartment: <MdApartment />,
+    store: <BiStore />,
+    office: <GiOfficeChair />,
+  };
   return (
-    <div>Card</div>
-  )
-}
+    <div className={styles.container}>
+      <div className={styles.icon}>{icons[category]}</div>
+      <p className={styles.title}>{title}</p>
+      <p className={styles.location}>
+        <HiOutlineLocationMarker />
+        {location}
+      </p>
+      <span>{sp(price)} تومان</span>
+      <Link href={}>
+        مشاهده آگهی
+        <BiLeftArrowAlt />
+      </Link>
+    </div>
+  );
+};
 
-export default Card
+export default Card;
--- src/components/module/DashboardCard.js
+++ src/components/module/DashboardCard.js
@@ -1,10 +1,12 @@
-import styles from './DashboardCard.module.css'
-import Card from './Card'
+import styles from "./DashboardCard.module.css";
+import Card from "./Card";
 
 const DashboardCard = ({data}) => {
   return (
-    <div><Card data={data} /> </div>
-  )
-}
+    <div>
+      <Card data={data} />
+    </div>
+  );
+};
 
-export default DashboardCard
+export default DashboardCard;
