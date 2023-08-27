8a741dd47720af2aa852dacfaa7ecb3f0623a01b
mfghir <fatemeghafari77@gmail.com>
2023-08-27 00:15:17 -0700

    add PATCH for ads editing

--- src/app/api/profile/route.js
+++ src/app/api/profile/route.js
@@ -1,3 +1,4 @@
+import Profile from "@/models/Profile";
 import User from "@/models/User";
 import connectDB from "@/utils/connectDB";
 import { Types } from "mongoose";
@@ -9,7 +10,6 @@ export async function POST(req) {
   try {
     await connectDB();
 
-    const body = await req.json();
     const {
       title,
       description,
@@ -21,7 +21,7 @@ export async function POST(req) {
       category,
       rules,
       amenities,
-    } = body;
+    } = await req.json();
 
     const session = getServerSession(req);
     if (!session) {
@@ -31,7 +31,7 @@ export async function POST(req) {
       );
     }
 
-    const user = await User.findOne({ email: session.user.email })
+    const user = await User.findOne({ email: session.user.email });
     if (!user) {
       return NextResponse.json(
         { error: "حساب کاربری یافت نشد" },
@@ -70,10 +70,91 @@ export async function POST(req) {
     });
 
     console.log(newProfile);
+    return NextResponse.json({ error: "آگهی جدید اضافه شد" }, { status: 201 });
+  } catch (err) {
+    console.log(err);
     return NextResponse.json(
-        { error: "آگهی جدید اضافه شد" },
-        { status: 201 }
-      ); 
+      { error: "مشکلی در سرور رخ داده است" },
+      { status: 500 }
+    );
+  }
+}
+
+export async function PATCH(req) {
+  try {
+    await connectDB();
+
+    const {
+      _id,
+      title,
+      description,
+      location,
+      phone,
+      price,
+      realState,
+      constructionDate,
+      category,
+      rules,
+      amenities,
+    } = await req.json();
+
+    const session = getServerSession(req);
+    if (!session) {
+      return NextResponse.json(
+        { error: "لطفا وارد حساب کاربری خود شوید" },
+        { status: 401 }
+      );
+    }
+
+    const user = await User.findOne({ email: session.user.email });
+    if (!user) {
+      return NextResponse.json(
+        { error: "حساب کاربری یافت نشد" },
+        { status: 404 }
+      );
+    }
+
+    if (
+      !_id ||
+      !title ||
+      !description ||
+      !location ||
+      !phone ||
+      !price ||
+      !realState ||
+      !constructionDate ||
+      !category
+    ) {
+      return NextResponse.json(
+        { error: "لطفا اطلاعات معتبر وارد کنید" },
+        { status: 400 }
+      );
+    }
+
+    const profile = await Profile.findOne({ _id });
+    if (!user._id.equals(profile.userId)) {
+      return NextResponse.json(
+        { error: "دسترسی شما به این آگهی محدود شده است" },
+        { status: 403 }
+      );
+    }
+
+    profile.title = title;
+    profile.description = description;
+    profile.location = location;
+    profile.phone = phone;
+    profile.price = price;
+    profile.realState = realState;
+    profile.constructionDate = constructionDate;
+    profile.amenities = amenities;
+    profile.rules = rules;
+    profile.category = category;
+    profile.save();
+
+    return NextResponse.json(
+      { error: "آگهی با موفقیت ویرایش شد" },
+      { status: 200 }
+    );
 
   } catch (err) {
     console.log(err);
