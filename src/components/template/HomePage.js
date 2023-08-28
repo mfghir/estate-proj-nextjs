864c562fe95ce7da4c5d873b4014bb2a59c4fbce
mfghir <fatemeghafari77@gmail.com>
2023-08-27 23:26:10 -0700

    done styling HomePage

--- src/components/template/HomePage.js
+++ src/components/template/HomePage.js
@@ -2,6 +2,7 @@ import CategoryCard from "@/module/CategoryCard";
 import styles from "./HomePage.module.css";
 import { FiCircle } from "react-icons/fi";
 
+import { FaCity } from "react-icons/fa";
 
 const HomePage = () => {
   const services = ["خرید", "فروش", "رهن", "اجاره"];
@@ -32,11 +33,22 @@ const HomePage = () => {
         </div>
       </div>
       <div className={styles.banner}>
-
         <CategoryCard title="خانه ویلایی" name="villa" />
         <CategoryCard title="آپارتمان" name="apartment" />
         <CategoryCard title="مغازه" name="store" />
         <CategoryCard title="دفتر" name="office" />
+      </div>
+
+      <div className={styles.city}>
+        <h3>شهر های پر بازدید</h3>
+        <ul>
+          {cities.map((i) => (
+            <li key={i}>
+              <FaCity />
+              <span>{i}</span>
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
   );
