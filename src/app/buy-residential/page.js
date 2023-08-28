6361eb511a70b2e67cc83a0df5e5ebb2c087f97b
mfghir <fatemeghafari77@gmail.com>
2023-08-28 00:15:28 -0700

    filtering data in BuyResidential

--- src/app/buy-residential/page.js
+++ src/app/buy-residential/page.js
@@ -1,6 +1,6 @@
 import BuyResidentialPage from "@/template/BuyResidentialPage";
 
-const BuyResidential = async () => {
+const BuyResidential = async ({ searchParams }) => {
   // next when the web is put on the host, http://localhost:3000/ must change to min address of site
   // بهتر است در کامپوننت های سرورساید از ای پی آی روت استفاده نکنیم(این مورد حالت تمرینی دارد)
   const res = await fetch("http://localhost:3000/api/profile", {
@@ -8,9 +8,14 @@ const BuyResidential = async () => {
   });
   const data = await res.json();
 
-  if(data.error) return <h3>مشکلی پیش آمده است</h3>
+  if (data.error) return <h3>مشکلی پیش آمده است</h3>;
 
-  return <BuyResidentialPage data={data.data} />;
+  let finalData = data.data;
+  if (searchParams.category) {
+    finalData = finalData.filter((i) => i.category === searchParams.category);
+  }
+
+  return <BuyResidentialPage data={finalData}  />;
 };
 
 export default BuyResidential;
