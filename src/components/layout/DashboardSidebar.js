0fafb617fd3f8994de58abf112d194021a064581
mfghir <fatemeghafari77@gmail.com>
2023-08-30 00:37:00 -0700

    change sidebar for admin role

--- src/components/layout/DashboardSidebar.js
+++ src/components/layout/DashboardSidebar.js
@@ -1,23 +1,20 @@
-import { getServerSession } from "next-auth";
 import styles from "./DashboardSidebar.module.css";
 import { CgProfile } from "react-icons/cg";
-
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import Link from "next/link";
 
-const DashboardSidebar = async ({ children }) => {
-  const session = await getServerSession(authOptions);
-
+const DashboardSidebar = async ({ children, email, role }) => {
   return (
     <div className={styles.container}>
       <div className={styles.sidebar}>
         <CgProfile />
-        <p>{session?.user.email}</p>
+        <>{role === "ADMIN" ? "ادمین" : null}</>
+        <p>{email}</p>
         <span></span>
 
         <Link href="/dashboard">حساب کاربری</Link>
         <Link href="/dashboard/my-profiles">آگهی های من</Link>
         <Link href="/dashboard/add">ثبت آگهی</Link>
+        <>{role === "ADMIN" ? <Link href="/admin">در انتظار تایید</Link> : null}</>
       </div>
 
       <div className={styles.main}>{children}</div>
