75d9dd7eba8a2a19cbbb7600b9bba25a59ac4d41
mfghir <fatemeghafari77@gmail.com>
2023-08-30 00:15:33 -0700

    identifying user role

--- src/app/dashboard/layout.js
+++ src/app/dashboard/layout.js
@@ -3,11 +3,19 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "../api/auth/[...nextauth]/route";
 
 import { redirect } from "next/navigation";
+import connectDB from "@/utils/connectDB";
+import User from "@/models/User";
 
 const DashboardLayout = async ({ children }) => {
   const session = await getServerSession(authOptions);
   if (!session) redirect("/signin");
-  return <DashboardSidebar>{children}</DashboardSidebar>;
+
+  await connectDB();
+  const user = await User.findOne({ email: session.user.email });
+
+  if(!user) return <h3>مشکلی پیش آمده است</h3>
+
+  return <DashboardSidebar role={user.role} email={user.email} >{children}</DashboardSidebar>;
 };
 
 export default DashboardLayout;
